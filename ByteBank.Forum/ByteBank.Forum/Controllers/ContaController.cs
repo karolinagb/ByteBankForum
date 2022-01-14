102eebdd5df203ca23e4382e74406d1389e8cc35
x <karolina_bp@hotmail.com>
2022-01-14 18:07:57 -0300

    feat: Criação da página de Registro do usuário
    - Criado o controlador ContaController com as Actions Registrar Get e Post
    - Criado a view Model de Registro de Usuário
    - Criada a View com o formulário de registro
    - Criado um nav item para acesso ao registro de usuário

---
+++ ByteBank.Forum/ByteBank.Forum/Controllers/ContaController.cs
@@ -0,0 +1,19 @@
+﻿using ByteBank.Forum.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ByteBank.Forum.Controllers
+{
+    public class ContaController : Controller
+    {
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(ContaRegistrarViewModel model)
+        {
+            return View();
+        }
+    }
+}
