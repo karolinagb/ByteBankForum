088f2a789e5e9bee0b4aeb2a5c5d953a987e65f3
x <karolina_bp@hotmail.com>
2022-01-17 12:07:37 -0300

    Implementação do Identity na aplicação
    1) Foi criada uma connection string no appsetting.json para mysql
    
    2) Foi adicionadoos pacotes Microsoft.EntityFrameworkCore.Tools para gerar comando de migração no console do gerenciador de pacites e Pomelo.EntityFrameworkCore.MySql para configurar o provider do banco para mysql
    
    3) Foi criada um model UsuarioAplicacao que herda de IdentityUser a fim de personalizar nosso usuario acrescentando o campo NomeCompleto
    
    4) Foi feita a configuração da nossa classe startup para adicionar o serviço do contexto do banco de dados usando mysql, adicionamos o serviço do identity usando nossa classe personalizada de user e foi adicionado a autenticação ao pipeline da aplicação.
    
    5) Depois disso foi feita a injeção dos serviços UserManager (que gerencia o nosso usuario no banco) e so SigInManager no construtor do controlador ContaController. Foi implementado também o método de Registro de usuário.

---
+++ ByteBank.Forum/ByteBank.Forum/Data/ByteBankForumContext.cs
@@ -0,0 +1,19 @@
+﻿using ByteBank.Forum.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ByteBank.Forum.Data
+{
+    public class ByteBankForumContext : IdentityDbContext<UsuarioAplicacao>
+    {
+        public ByteBankForumContext(DbContextOptions<ByteBankForumContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ ByteBank.Forum/ByteBank.Forum/Models/UsuarioAplicacao.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ByteBank.Forum.Models
+{
+    public class UsuarioAplicacao : IdentityUser
+    {
+        public string NomeCompleto { get; set; }
+    }
+}
