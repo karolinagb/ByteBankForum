498bab798d1492a734562e294610c1125d2970b1
x <karolina_bp@hotmail.com>
2022-01-27 09:42:06 -0300

    feat: Criação de um tópico no fórum
    Implementação da action Get e suas respectiva View

---
+++ ByteBank.Forum/ByteBank.Forum/ViewModels/TopicoCriarViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ByteBank.Forum.ViewModels
+{
+    public class TopicoCriarViewModel
+    {
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+    }
+}
