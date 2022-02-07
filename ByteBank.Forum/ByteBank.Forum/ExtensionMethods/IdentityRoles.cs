caccab52ecc2b39b4b80b0b795ce9bc3ca43d905
x <karolina_bp@hotmail.com>
2022-02-07 17:15:32 -0300

    feat: Criados perfis de moderador e admin e o usuario administrador do sistema

--- ByteBank.Forum/ByteBank.Forum/ExtensionMethods/IdentityRoles.cs
+++ ByteBank.Forum/ByteBank.Forum/ExtensionMethods/IdentityRoles.cs
@@ -1,4 +1,5 @@
 ﻿using ByteBank.Forum.Data;
+using ByteBank.Forum.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -28,19 +29,42 @@ namespace ByteBank.Forum.ExtensionMethods
 
         public static async Task CriarRolesAsync(IServiceProvider serviceProvider, IConfiguration configuration)
         {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            var userManager = serviceProvider.GetRequiredService<UserManager<UsuarioAplicacao>>();
 
             string[] roleNames = { RolesNames.ADMIN, RolesNames.MODERADOR };
 
             foreach (var role in roleNames)
             {
-                var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
                 var roleExist = await roleManager.RoleExistsAsync(role);
 
                 if (!roleExist)
                 {
                     await roleManager.CreateAsync(new IdentityRole(role));
                 }
+            }
 
+            var powerUser = new UsuarioAplicacao
+            {
+                UserName = configuration.GetSection("UserSettings")["UserName"],
+                Email = configuration.GetSection("UserSettings")["UserEmail"],
+                EmailConfirmed = true
+            };
+
+            var userPassword = configuration.GetSection("UserSettings")["UserPassword"];
+
+            var user = await userManager.FindByEmailAsync(powerUser.Email);
+
+            if (user == null)
+            {
+                var result = await userManager.CreateAsync(powerUser, userPassword);
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(powerUser, RolesNames.ADMIN);
+                }
             }
         }
     }
